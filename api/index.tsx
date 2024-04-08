25007dfa1d253e52f88a4c4c01d878762b0d2c6a
Alexander Gilbertson <alexandertgilbertson@gmail.com>
2024-04-08 10:55:24 -0500

    fixed random image

--- api/index.tsx
+++ api/index.tsx
@@ -3,9 +3,6 @@ import { devtools } from "frog/dev";
 import { serveStatic } from "frog/serve-static";
 // import { neynar } from 'frog/hubs'
 import { handle } from "frog/vercel";
-import { createSystem } from "frog/ui";
-
-const { Image } = createSystem;
 
 // Uncomment to use Edge Runtime.
 // export const config = {
@@ -55,56 +52,59 @@ app.frame("/", (c) => {
       </div>
     ),
     intents: [
-      <Button value="higher" action="/higher">
+      <Button value="higher" action="/higher0">
         Higher
       </Button>,
     ],
   });
 });
 
-app.frame("/higher", async (c) => {
-  const randomInt = Math.floor(Math.random() * 100);
-  const imageUrlBase = `https://picsum.photos/seed/${randomInt}/1200/630`;
-  return c.res({
-    image: (
-      <div
-        style={{
-          alignItems: "center",
-          backgroundImage: `url(${imageUrlBase})`,
-          display: "flex",
-          flexDirection: "column",
-          flexWrap: "nowrap",
-          height: "100%",
-          justifyContent: "center",
-          textAlign: "center",
-          width: "100%",
-        }}
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="449.868"
-          height="481.305"
-          viewBox="0 0 449.868 481.305"
-          fill="none"
+for (let i = 0; i < 200; i++) {
+  app.frame(`/higher${i}`, (c) => {
+    const randomInt = Math.floor(Math.random() * 200);
+    const imageUrlBase = `https://picsum.photos/seed/${i}/1200/630`;
+    return c.res({
+      image: (
+        <div
+          style={{
+            alignItems: "center",
+            backgroundImage: `url(${imageUrlBase})`,
+            display: "flex",
+            flexDirection: "column",
+            flexWrap: "nowrap",
+            height: "100%",
+            justifyContent: "center",
+            textAlign: "center",
+            width: "100%",
+          }}
         >
-          <path
-            fill="#fff"
-            fillOpacity="0.69"
-            d="M225.011.305l224.857 224.857-66.452 66.453L158.558 66.757 225.011.305zm-48.453 481v-375h97v375h-97zM0 224.857L224.857 0l66.453 66.453L66.453 291.31 0 224.857z"
-          ></path>
-        </svg>
-      </div>
-    ),
-    intents: [
-      <Button value="back" action="/">
-        Back
-      </Button>,
-      <Button.Link href="https://higher-nine.vercel.app/api/higher?accept=text/html">
-        View Image
-      </Button.Link>,
-    ],
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="449.868"
+            height="481.305"
+            viewBox="0 0 449.868 481.305"
+            fill="none"
+          >
+            <path
+              fill="rgba(255, 255, 255, 0.69)"
+              d="M225.011.305l224.857 224.857-66.452 66.453L158.558 66.757 225.011.305zm-48.453 481v-375h97v375h-97zM0 224.857L224.857 0l66.453 66.453L66.453 291.31 0 224.857z"
+            ></path>
+          </svg>
+        </div>
+      ),
+      intents: [
+        <Button value="higher" action={`/higher${randomInt}`}>
+          Keep Going Higher
+        </Button>,
+        <Button.Link
+          href={`https://higher-nine.vercel.app/api/higher${i}?accept=text/html`}
+        >
+          View Image
+        </Button.Link>,
+      ],
+    });
   });
-});
+}
 
 // @ts-ignore
 const isEdgeFunction = typeof EdgeFunction !== "undefined";
